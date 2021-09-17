331996fed59a89cc935e7fd7ca587c362d4d4825
Rushikesh Bhattad <rushikeshbhattad8@gmail.com>
2021-09-17 16:35:41 +0530

    Updated

--- TrainingAcademyAPIDemo/Program.cs
+++ TrainingAcademyAPIDemo/Program.cs
@@ -21,63 +21,101 @@ namespace TrainingAcademyAPIDemo
             String Course_Fee;
             String Trainer_Name;
             int Student_Marks;
+            int rollno;
             Connection1.CreateConnection();
             Console.WriteLine("Connection with database Successful");
             Console.ReadKey();
 
-            Console.WriteLine("Display Data");
-            
-            Connection1.DisplayData();
-            
-            Console.WriteLine("Display Top5 StudentData");
-            
-            Connection1.DisplayTop5();
 
-            Console.WriteLine("Top Student Trainer and Course_Name");
-            Connection1.DisplayAverage();
+            do
+            {
 
-            Console.WriteLine("Enter your Details");
-            Console.WriteLine("Enter the Rollno");
-            Student_Rollno = Convert.ToInt32(Console.ReadLine());
 
-            Console.WriteLine("enter the StudentName");
-            StudentName = Console.ReadLine();
+                Console.WriteLine("Press 1 For Display Data");
+                Console.WriteLine("Press 2 For Insert Data");
+                Console.WriteLine("Press 3 For Update Data");
+                Console.WriteLine("Press 4 For Delete Data");
+                Console.WriteLine("Press 5 For Display Top 5 Student Data");
+                Console.WriteLine("Press 6 For Display Top 5 Course Name and Trainer Name Data");
+                Console.WriteLine("Press 7 for exit");
 
-            Console.WriteLine("enter the Address");
-            Address1 = Console.ReadLine();
+                int ch = Convert.ToInt32(Console.ReadLine());
 
-            Console.WriteLine("enter the Gender");
-            Gender = Console.ReadLine();
+                switch (ch)
+                {
+                    case 1:
+                        Console.WriteLine("Display Data");
+                        Connection1.DisplayData();
+                        break;
 
-            Console.WriteLine("enter the ContactNo");
-            Contact_No = Console.ReadLine();
- 
-            Console.WriteLine("enter the CourseName");
-            Course_Name = Console.ReadLine();
-            
-            Console.WriteLine("enter the Course_Fee");
-            Course_Fee = Console.ReadLine();
-            
-            Console.WriteLine("enter the TrainerName");
-            Trainer_Name = Console.ReadLine();
-            
-            Console.WriteLine("Enter the Marks");
-            Student_Marks= Convert.ToInt32(Console.ReadLine());
-           
-            Connection1.InsertData(Student_Rollno,  StudentName,  Address1,  Gender,  Contact_No,  Course_Name,  Course_Fee, Trainer_Name ,Student_Marks);
+                    case 2:
+                        Console.WriteLine("Enter your Details");
+                        Console.WriteLine("Enter the Rollno");
+                        Student_Rollno = Convert.ToInt32(Console.ReadLine());
 
-            int rollno;
-            Console.WriteLine("enter rollno");
-            rollno = Convert.ToInt32(Console.ReadLine());
-            Connection1.UpdateData(rollno);
+                        Console.WriteLine("enter the StudentName");
+                        StudentName = Console.ReadLine();
 
-            
-            
-            Console.WriteLine("Enter the Rollno you want to delete");
-            Student_Rollno = Convert.ToInt32(Console.ReadLine());
-            Connection1.DeleteData(rollno);
-            
-            
+                        Console.WriteLine("enter the Address");
+                        Address1 = Console.ReadLine();
+
+                        Console.WriteLine("enter the Gender");
+                        Gender = Console.ReadLine();
+
+                        Console.WriteLine("enter the ContactNo");
+                        Contact_No = Console.ReadLine();
+
+                        Console.WriteLine("enter the CourseName");
+                        Course_Name = Console.ReadLine();
+
+                        Console.WriteLine("enter the Course_Fee");
+                        Course_Fee = Console.ReadLine();
+
+                        Console.WriteLine("enter the TrainerName");
+                        Trainer_Name = Console.ReadLine();
+
+                        Console.WriteLine("Enter the Marks");
+                        Student_Marks = Convert.ToInt32(Console.ReadLine());
+
+                        Connection1.InsertData(Student_Rollno, StudentName, Address1, Gender, Contact_No, Course_Name, Course_Fee, Trainer_Name, Student_Marks);
+                        break;
+
+                    case 3:
+                        Console.WriteLine("enter rollno");
+                        rollno = Convert.ToInt32(Console.ReadLine());
+                        Connection1.UpdateData(rollno);
+                        break;
+
+                    case 4:
+                        Console.WriteLine("Enter the Rollno you want to delete");
+                        Student_Rollno = Convert.ToInt32(Console.ReadLine());
+                        Connection1.DeleteData(Student_Rollno);
+                        break;
+
+                    case 5:
+                        Console.WriteLine("Display Top5 StudentData");
+                        Connection1.DisplayTop5();
+                        break;
+
+                    case 6:
+                        Console.WriteLine("Top Student Trainer and Course_Name");
+                        Connection1.DisplayAverage();
+                        break;
+
+
+                    case 7:
+                        break;
+
+                    default:
+                        Console.WriteLine("Enter correct option");
+                        break;
+
+
+                }
+            } while (true);
+
+
+
         }
-    }
+        }
 }
